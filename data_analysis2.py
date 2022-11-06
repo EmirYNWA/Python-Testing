b3a4291e18f9e5bf7f25701701373a225d0cff85
EmirYNWA <emirhalilovic2005@gmail.com>
2022-11-06 12:33:09 +0100

    added plot of square root of reciprocal exponential function

--- data_analysis2.py
+++ data_analysis2.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     inv_e_pow_x = np.exp(-x**2)
 
     plt.plot(x, inv_e_pow_x, 'g-', label='e^(-x)')
+    plt.plot(x, np.sqrt(inv_e_pow_x), 'r-', label="sqrt(e^(-x))")
 
     plt.legend(loc=9)
     plt.show()
